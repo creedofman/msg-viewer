3136980c08ab4a297bf254ca72bc58fe1958770f
Alexander Ganahl <alex.ganahl@gmail.com>
2025-08-06 12:49:59 -0400

    chore: remove extra docs and tracking

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,7 +2,3 @@ declare module "*.html" {
   const content: string;
   export default content;
 }
-
-interface Window {
-  gtag: (event: string, type: string, args: { [key: string]: any }) => void;
-}
